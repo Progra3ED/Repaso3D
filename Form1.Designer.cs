c7cada58052c65e05123a0db2c9fc852e48fb729
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2022-03-14 10:41:32 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Repaso3D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonResumen = new System.Windows.Forms.Button();
+            this.buttonOrdenar = new System.Windows.Forms.Button();
+            this.labelMayor = new System.Windows.Forms.Label();
+            this.labelMenor = new System.Windows.Forms.Label();
+            this.buttonMayorMenor = new System.Windows.Forms.Button();
+            this.labelNombre = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(450, 210);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonResumen
+            // 
+            this.buttonResumen.Location = new System.Drawing.Point(52, 306);
+            this.buttonResumen.Name = "buttonResumen";
+            this.buttonResumen.Size = new System.Drawing.Size(132, 70);
+            this.buttonResumen.TabIndex = 1;
+            this.buttonResumen.Text = "Mostrar Resumen";
+            this.buttonResumen.UseVisualStyleBackColor = true;
+            this.buttonResumen.Click += new System.EventHandler(this.buttonResumen_Click);
+            // 
+            // buttonOrdenar
+            // 
+            this.buttonOrdenar.Enabled = false;
+            this.buttonOrdenar.Location = new System.Drawing.Point(229, 306);
+            this.buttonOrdenar.Name = "buttonOrdenar";
+            this.buttonOrdenar.Size = new System.Drawing.Size(139, 70);
+            this.buttonOrdenar.TabIndex = 2;
+            this.buttonOrdenar.Text = "Ordenar por Cuota";
+            this.buttonOrdenar.UseVisualStyleBackColor = true;
+            this.buttonOrdenar.Click += new System.EventHandler(this.buttonOrdenar_Click);
+            // 
+            // labelMayor
+            // 
+            this.labelMayor.AutoSize = true;
+            this.labelMayor.Location = new System.Drawing.Point(597, 62);
+            this.labelMayor.Name = "labelMayor";
+            this.labelMayor.Size = new System.Drawing.Size(35, 13);
+            this.labelMayor.TabIndex = 3;
+            this.labelMayor.Text = "label1";
+            // 
+            // labelMenor
+            // 
+            this.labelMenor.AutoSize = true;
+            this.labelMenor.Location = new System.Drawing.Point(600, 153);
+            this.labelMenor.Name = "labelMenor";
+            this.labelMenor.Size = new System.Drawing.Size(35, 13);
+            this.labelMenor.TabIndex = 4;
+            this.labelMenor.Text = "label2";
+            // 
+            // buttonMayorMenor
+            // 
+            this.buttonMayorMenor.Enabled = false;
+            this.buttonMayorMenor.Location = new System.Drawing.Point(426, 306);
+            this.buttonMayorMenor.Name = "buttonMayorMenor";
+            this.buttonMayorMenor.Size = new System.Drawing.Size(162, 70);
+            this.buttonMayorMenor.TabIndex = 5;
+            this.buttonMayorMenor.Text = "Mostrar Mayor Menor";
+            this.buttonMayorMenor.UseVisualStyleBackColor = true;
+            this.buttonMayorMenor.Click += new System.EventHandler(this.buttonMayorMenor_Click);
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(597, 40);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(35, 13);
+            this.labelNombre.TabIndex = 6;
+            this.labelNombre.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 450);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.buttonMayorMenor);
+            this.Controls.Add(this.labelMenor);
+            this.Controls.Add(this.labelMayor);
+            this.Controls.Add(this.buttonOrdenar);
+            this.Controls.Add(this.buttonResumen);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonResumen;
+        private System.Windows.Forms.Button buttonOrdenar;
+        private System.Windows.Forms.Label labelMayor;
+        private System.Windows.Forms.Label labelMenor;
+        private System.Windows.Forms.Button buttonMayorMenor;
+        private System.Windows.Forms.Label labelNombre;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso3D
+{
+    public partial class Form1 : Form
+    {
+        List<Propiedad> propiedades = new List<Propiedad>();
+        List<Propietario> propietarios = new List<Propietario>();   
+        List<Resumen> resumen = new List<Resumen>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarPropiedades()
+        {         
+            
+            FileStream stream = new FileStream("Propiedades.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Propiedad propiedad = new Propiedad();
+                propiedad.numeroCasa = reader.ReadLine();
+                propiedad.dpi = reader.ReadLine();
+                propiedad.cuota = Convert.ToDecimal(reader.ReadLine());
+
+                propiedades.Add(propiedad);
+            }
+            reader.Close();
+        }
+
+        private void CargarPropietarios()
+        {
+            FileStream stream = new FileStream("Propietarios.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Propietario propietario = new Propietario();
+                propietario.dpi = reader.ReadLine();
+                propietario.nombre = reader.ReadLine();
+                propietario.apellido = reader.ReadLine();
+
+                propietarios.Add(propietario);
+            }
+
+            reader.Close();
+
+        }
+
+        private void CargarGridview()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = resumen;
+            dataGridView1.Refresh();
+        }
+
+        private void buttonResumen_Click(object sender, EventArgs e)
+        {
+            CargarPropiedades();
+            CargarPropietarios();
+
+            for (int i = 0; i < propiedades.Count; i++)
+            {
+                for (int j = 0; j < propietarios.Count; j++)
+                {
+                    if (propiedades[i].dpi == propietarios[j].dpi)
+                    { 
+                        Resumen datoResumen = new Resumen();
+                        datoResumen.nombre = propietarios[j].nombre;
+                        datoResumen.apellido = propietarios[j].apellido;
+                        datoResumen.numeroCasa = propiedades[i].numeroCasa;
+                        datoResumen.cuota = propiedades[i].cuota;
+
+                        resumen.Add(datoResumen);
+                    }
+                }
+            }
+
+           CargarGridview();
+
+            buttonOrdenar.Enabled = true;
+        }
+
+        private void buttonOrdenar_Click(object sender, EventArgs e)
+        {
+            resumen = resumen.OrderBy(c => c.cuota).ToList(); ;
+            CargarGridview();
+
+            buttonMayorMenor.Enabled = true;
+            
+        }
+
+        private void buttonMayorMenor_Click(object sender, EventArgs e)
+        {
+            labelMenor.Text = resumen[0].cuota.ToString();
+
+            int cuantos = resumen.Count();
+            labelMayor.Text = resumen[cuantos - 1].cuota.ToString();
+            labelNombre.Text = resumen[cuantos - 1].nombre + ", " + resumen[cuantos - 1].apellido;
+
+        }
+    }
+}
